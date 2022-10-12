f7377c4e252c61c1b7ac75a714f1161c3c8d922f
shindeAkshu <sakshata416@gmail.com>
2022-10-12 11:24:51 -0700

    Youtube clone

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 
 class Home  extends React.Component {
     constructor(props) {
-        super(props);
+        
         this.state = {
             categoryIndex : 0,
         };
