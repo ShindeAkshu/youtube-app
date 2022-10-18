f2d4231445e1fe5425fe7d22d98c146528f78b6d
shindeAkshu <sakshata416@gmail.com>
2022-10-18 06:42:00 -0700

    Youtube Clone

---
+++ src/containers/HeaderNav/HeaderNav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+
+export function HeaderNav(props){
+    return(
+        
+            <div className='Container'>
+              <div>Home</div>
+              <div>Search</div>
+              <div>Library</div>
+                
+            </div>
+        
+    );
+    
+}
+
+export default HeaderNav;
