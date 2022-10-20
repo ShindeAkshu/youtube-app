1e26984622b7c5728af45a68456e60ca5bdc9b8d
shindeAkshu <sakshata416@gmail.com>
2022-10-20 10:32:31 -0700

    Youtube clone

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -3,3 +3,15 @@ import './VideoGrid.css';
 import { VideoPreview } from '../VideoPreview/VideoPreview';
 import {VideoGridHeader} from './VideoGridHeader/VideoGridHeader';
 import {Divider} from 'semantic-ui-react';
+
+export function VideoGird(props) {
+    if(!props.videos || props.videos.length){
+        <div/>;
+    }
+    const gridItems =props.videos.map(video =>{
+        return (<VideoPreview video={video}
+                                key={video.id}
+                                pathname='/watch'
+                                search={`?v=${video.id}`}/>)
+    })
+}
