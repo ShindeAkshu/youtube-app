09a6ca7439e57db1cea2b61fae8fc007da13e085
shindeAkshu <sakshata416@gmail.com>
2022-10-20 10:21:59 -0700

    Youtube clone

--- src/components/VideoGrid/VideoGrid.js
+++ src/components/VideoGrid/VideoGrid.js
@@ -1,1 +1,5 @@
 import React from 'react';
+import './VideoGrid.css';
+import { VideoPreview } from '../VideoPreview/VideoPreview';
+import {VideoGridHeader} from './VideoGridHeader/VideoGridHeader';
+import {Divider} from 'semantic-ui-react';
