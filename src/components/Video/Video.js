8ec6439ae370591102e19624b3f39dbfcd56e7b8
shindeAkshu <sakshata416@gmail.com>
2022-10-26 11:15:31 -0700

    Youtube clone

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -11,7 +11,10 @@ export function Video(props){
     const embedURL = `${BASE_EMBED_URL}${props.id}`;
     return(
         <div className='video-container'>
-            
+            <div className="video">
+                <iframe className='video-player' src={embedUrl} frameBorder='0'
+                allow='autoplay; encrypted-media' allowFullScreen title='video'/> 
+            </div>
         </div>
     )
 }
