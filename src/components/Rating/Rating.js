f658df7ad673b532d440494958610d5cf3400392
shindeAkshu <sakshata416@gmail.com>
2022-11-01 11:12:25 -0700

    Youtube clone

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Rating.css';
 import {Icon,Progress} from 'semantic-ui-react';
-
+import {getShortNumberString} from '../../services/number/number-fromat';
 
 export function Rating(props){
     let rating =null;
